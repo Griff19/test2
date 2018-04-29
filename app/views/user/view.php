5f25c7b8b0565ed7cc781c406700f9782fb93dd9
griff19 <griff19@mail.ru>
2018-04-29 22:12:07 +0700

    адаптация для работы из подкаталога

--- app/views/user/view.php
+++ app/views/user/view.php
@@ -1,13 +1,13 @@
 <?php
 use core\T;
-
+use core\App;
 /**
  * @var $user models\User
  */
 ?>
 <h1><?= T::t('PAGE_PROFILE') ?> "<?= $user->login ?>"</h1>
 <div class="img-conteiner">
-    <img class="img-profile" src="/app/<?= $user->link_file ?>" alt="<?= $user->snp ?>"/>
+    <img class="img-profile" src="<?= App::$root ?>app/<?= $user->link_file ?>" alt="<?= $user->snp ?>"/>
 </div>
 <div>
     <p><?= T::t('FULL_NAME')?>: <?= $user->snp ?></p>
