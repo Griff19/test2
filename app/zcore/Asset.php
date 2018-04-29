6b55a90cbe36c99fc821910b518b1568c4f403ac
griff19 <griff19@mail.ru>
2018-04-29 18:36:21 +0700

    адаптация для работы из подкаталога

--- app/zcore/Asset.php
+++ app/zcore/Asset.php
@@ -41,6 +41,6 @@ class Asset
      */
     public static function registerJsFile($file, $place = Asset::HEAD)
     {
-        Asset::$js[$file] = App::$root . $place;
+        Asset::$js[App::$root . $file] = $place;
     }
 }
